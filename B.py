3ae82f8c0631d4ee2680e9e08a3b4acb9f0a3c5b
Hanin Ainussyamsi Prabowo <haninsyamsi15@gmail.com>
2023-09-15 16:45:37 +0700

    Upload pengerjaan poin B

--- B.py
+++ B.py
@@ -1,60 +1,72 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 13 15:36:55 2023
+Created on Fri Sep 15 16:40:51 2023
 
 @author: 62853
 """
 
-#!/bin/python
-# -*- coding: utf-8 -*-
-
 import numpy as np
+from scipy import signal
 import matplotlib.pyplot as plt
 
-def myFFT(v):
-    n = len(v)
-    
-    if n==1:
-        return v
-    else:
-        # implement some recursive
-        F_even = myFFT(v[::2])
-        F_odd = myFFT(v[1::2])
-        
-        # frequency factor
-        fac = np.exp(-2j*np.pi*np.arange(n)/n)
-        
-        # build FFT array
-        F = np.concatenate([
-            F_even + fac[:int(n/2)]*F_odd,
-            F_even + fac[int(n/2):]*F_odd
-            ])
-        
-        return F
+# Generate a sample signal
+fs = 1000  # Sampling frequency (Hz)
+t = np.linspace(0, 1, fs, endpoint=False)  # Time vector
+freq = 5  # Frequency of the signal (Hz)
+signal_waveform = np.sin(2 * np.pi * freq * t)
 
-# ID
-print("Nama: Achmadi ST MT")
-print("NRP: 2410100085")
+# Add noise to the signal
+noise = np.random.normal(0, 0.5, fs)
+noisy_signal = signal_waveform + noise
 
-# X array linear spacing
-X = np.arange(0,1,1.0/128)
+# Design a low-pass Butterworth filter
+cutoff_frequency = 10  # Cutoff frequency (Hz)
+order = 4  # Filter order
+b, a = signal.butter(order, cutoff_frequency / (fs / 2), 'low')
 
-# Y sinus function
-Y = np.sin(2*np.pi*X)
+# Apply the filter to the noisy signal
+filtered_signal = signal.lfilter(b, a, noisy_signal)
 
-# create noise array at X length
-R = np.random.rand(len(X))
+# Perform FFT on the filtered signal
+fft_result = np.fft.fft(filtered_signal)
+frequencies = np.fft.fftfreq(len(fft_result), 1/fs)
 
-# add noise to sine result
-Yr = Y + R
+# Create a modulation signal
+modulation_frequency = 2  # Modulation frequency (Hz)
+modulation_signal = np.sin(2 * np.pi * modulation_frequency * t)
 
-# FFT all
-FY = np.abs(myFFT(Y))
-FYr = np.abs(myFFT(Yr))
+# Perform convolution between the filtered signal and the modulation signal
+convolution_result = np.convolve(filtered_signal, modulation_signal, 'same')
 
-# plot all
-fig, ax = plt.subplots(2,2)
-ax[0,0].plot(X,Y)
-ax[0,1].plot(FY)
-ax[1,0].plot(X,Yr)
-ax[1,1].plot(FYr)
+# Plot the original, noisy, and filtered signals
+plt.figure(figsize=(12, 6))
+plt.subplot(3, 1, 1)
+plt.plot(t, signal_waveform)
+plt.title("Original Signal")
+
+plt.subplot(3, 1, 2)
+plt.plot(t, noisy_signal)
+plt.title("Noisy Signal")
+
+plt.subplot(3, 1, 3)
+plt.plot(t, filtered_signal)
+plt.title("Filtered Signal")
+
+plt.tight_layout()
+
+# Plot the FFT result
+plt.figure(figsize=(12, 6))
+plt.plot(frequencies, np.abs(fft_result))
+plt.title("FFT of Filtered Signal")
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Amplitude")
+
+# Plot the convolution result
+plt.figure(figsize=(12, 6))
+plt.plot(t, convolution_result)
+plt.title("Convolution Result")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+
+plt.show()
+
